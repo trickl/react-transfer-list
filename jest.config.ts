95bdb56a783a0c5fa5e389de343acf5bcf6f7c3b
Tim Gee <writetotimgee@gmail.com>
2021-10-31 18:55:03 +0000

    Add a simple test

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  transform: {
+    '.(ts|tsx)': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  rootDir: "src",
+};
+
+export default config;
---
+++ src/components/TransferList/ReorderableList.test.tsx
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+
+import { ReorderableList } from "./ReorderableList";
+
+describe("<ReorderableList />", () => {
+  test("Shows list items", () => {
+    const handleOnChange = jest.fn();
+    const list = render(<ReorderableList ids={["1", "2", "3"]} onChange={handleOnChange}/>);
+    expect(list.getByText("1")).toBeDefined();
+    expect(list.getByText("2")).toBeDefined();
+    expect(list.getByText("3")).toBeDefined();
+  });
+});
