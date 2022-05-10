5610cb636a4573f66a63be1da095876598845b92
Tim Gee <writetotimgee@gmail.com>
2022-05-10 21:38:10 +0100

    Upgrade to React 18

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,7 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-actions',
-    '@storybook/addon-essentials',
-  ],
+  addons: [],
   typescript: {
     check: false,
     checkOptions: {},
